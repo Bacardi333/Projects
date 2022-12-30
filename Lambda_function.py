68fad1d450438e957d315611f57c32ef62d4743a
PSS <Serg5551984@gmail.com>
2022-12-31 00:54:10 +0300

    Programm in which we use list comprehension

--- Lambda_function.py
+++
@@ -1,3 +0,0 @@
-numbers = [7, 22, 49, 701]
-num = list(map(lambda x: x // 7, numbers))
-print(num)
--- main.py
+++ main.py
@@ -1,2 +1,5 @@
+numbers = [int(_) for _ in input().split()]
+for i in range(0, len(numbers) - 1, 2):
+    numbers[i], numbers[i + 1] = numbers[i + 1], numbers[i]
 
-print([(letters, nums) for letters in 'abcd' for nums in range(4)])
+print(*numbers)
